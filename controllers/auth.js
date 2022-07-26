06922b92e1c7bed4cc29911819aa50be09bbfed3
Kamelia Karam <kamelia_karam@hotmail.com>
2022-07-26 21:12:22 +0700

    created get routes signup/login and post signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,43 @@
 // Packages
 const express = require('express')
 const router = express.Router()
+const Users = require ('../models/users')
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 
 router.get('/', (req, res) => {
-  res.send('Hello form login')
+  res.render('login')
 })
-router.get('/singup', (req, res) => {
-  res.send('Hello form signup')
+
+router.get('/signup', (req, res) => {
+  res.render('signup')
 })
 
 router.post('/login', async (req, res) => {})
-router.post('/singup', async (req, res) => {})
-// Export
+
+
+
+router.post('/signup', async(req, res, next) => {
+  let founduser= await Users.findOne({
+      email: req.body.email
+  })
+  try {
+      if (founduser) {
+          throw new Error('Account already exists')
+      } else {
+          let user = await Users.create(req.body)
+          req.login(user, (err) => {
+              if (err) {throw error}
+              if(user.car){
+                res.redirect('/orders') 
+              } else {
+                res.redirect('/orders/create')
+              }
+          })
+      }
+  } catch (err) {
+      next (err)
+  } 
+})// Export
 module.exports = router
