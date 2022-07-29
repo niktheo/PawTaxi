c3dbdb93123d4fd0047048723069596c6dae4426
niktheo <nikostheodoridis1999@gmail.com>
2022-07-29 11:32:43 +0300

    Create a map.

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,17 @@ var mapOptions = {
   mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 
+//create map
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions)
+
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService()
+
+//create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer()
+
+//bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map)
     } else {
     }
   })
