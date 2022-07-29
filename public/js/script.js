206ac0bc72138faf48ec2fd13751dbc0a49e9979
niktheo <nikostheodoridis1999@gmail.com>
2022-07-29 11:34:47 +0300

    Autocomplete the inputs from the category that you choose

--- public/js/script.js
+++ public/js/script.js
@@ -63,5 +63,13 @@ function calcRoute() {
   })
 }
 
+//create autocomplete objects for all inputs
+var options = {
+  types: ['(cities)']
 }
 
+var input1 = document.getElementById('from')
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options)
+
+var input2 = document.getElementById('to')
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options)
